21bd8b77da1ca874436d81599a7354f1283e2a89
Jeferson <jeferson.silva220@gmail.com>
2024-09-11 21:25:16 -0300

    Add files via upload

---
+++ teste3.js
@@ -0,0 +1,18 @@
+let fruta = "melanci";
+
+switch (fruta) {
+    case "maça":
+        console.log("Não vendemos frutas aqui");
+        break;
+    case "kiwi":
+        console.log("Estamos com escassez de kiwis");
+        break;
+    case "melancia":
+        console.log("Aqui está, são 3 reais o quilo")
+        break;
+
+    default:
+        console.log("Erro no console.")
+
+
+}
---
+++ teste4.js
@@ -0,0 +1,26 @@
+let carro = "hatch";
+
+switch (carro) {
+    case "hatch":
+        console.log("Compra efetuada com sucesso.");
+        document.write("Compra efetuada com sucesso.")
+        break;
+    case "sedan":
+        console.log("Tem cesteza que não prefere este modelo?");
+        document.write("Tem cesteza que não prefere este modelo?")
+        break;
+    case "motocicletas":
+        console.log("Tem cesteza que não prefere este modelo?")
+        document.write("Tem cesteza que não prefere este modelo?")
+        break;
+    case "caminhonetes":
+        console.log("Tem cesteza que não prefere este modelo?")
+        document.write("Tem cesteza que não prefere este modelo?")
+        break;
+
+    default:
+        console.log("Não trabalhamos com este tipo de automóvel aqui.")
+        document.write("Não trabalhamos com este tipo de automóvel aqui.")
+
+
+}
